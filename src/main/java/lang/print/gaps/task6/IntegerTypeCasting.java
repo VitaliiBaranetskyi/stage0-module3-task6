c674124fd1ec30aa78604936e82ae9477c54f5e4
Vitalii Baranetskyi <vitalik.baranetskiy@gmail.com>
2022-09-11 21:57:56 +0300

    Solved

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task6;
 
 public class IntegerTypeCasting {
     public static void main(String[] args) {
+        double first = 1000;
+        int second = (int) first;
+        System.out.println(second);
+        System.out.println(second);
     }
 }
